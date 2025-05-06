143fec13b287a229f5c40d1d2d1b47a91f10a71f
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2025-05-06 12:13:50 -0500

    feat: :hammer: practice event listener

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
 "use client"
 
 import Link  from "next/link"
-import { useState } from "react";
+import { useEffect,useState } from "react";
 
+      {/* inicia la funcion principal añade el evento de escucha para enter 
+        y añade el uso de los hooks useEffect,useStates en false muestra 
+        la pagina principal en true borra todo y renderiza solamente el hola mundo*/}
 
 export default function Home (){
+  const [empiezaFalse, contadorBoleano] = useState(false);
+
+  useEffect(() => {
+    const llave = (e:KeyboardEvent) => {
+      if (e.key === "Enter"){
+        contadorBoleano(true)
+      }
+    };
+    document.addEventListener("keydown", llave)
+
+
+return()=>{
+  document.removeEventListener("keydown", llave)
+
+};
+
+},[]);
+  if (empiezaFalse){
+    return <h1>hola mundo</h1>;
+  }
+      {/* renderizado pagina principal*/}
+
   return(
     <>
     <div className="ejm">
@@ -17,17 +42,22 @@ export default function Home (){
       <Conta/>
     </div>
     </>
-  )
+  );
+
+      {/* esto es lo que dibuja el boton contador y se lo añade a la pagina principal */}
+   function Conta(){
+
+    const [contador, setcontador] =useState(0);
+  
+    return(
+      <div className="conta">
+        <p>contador : {contador}</p>
+        <button onClick={()=> setcontador (contador+1)} className="contador">sumar</button>
+      </div>
+    )
+  }
+  
 }
 
-export function Conta(){
 
-  const [contador, setcontador] =useState(0);
 
-  return(
-    <div className="conta">
-      <p>contador : {contador}</p>
-      <button onClick={()=> setcontador (contador+1)} className="contador">sumar</button>
-    </div>
-  )
-}
